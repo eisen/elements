bd513ffda5879599fb621464a28d0f0a9030fd77
djowel <djowel@gmail.com>
2023-11-12 09:44:43 +0800

    Multi-window example

--- examples/multi_window/main.cpp
+++ examples/multi_window/main.cpp
@@ -8,8 +8,8 @@
 using namespace cycfi::elements;
 using namespace cycfi::artist;
 
-// Main window background color
 auto constexpr bkd_color = rgba(35, 35, 37, 255);
+constexpr auto bblue = colors::blue.opacity(0.4);
 auto background = box(bkd_color);
 
 int main(int argc, char* argv[])
@@ -17,18 +17,25 @@ int main(int argc, char* argv[])
    app _app(argc, argv, "Empty Starter", "com.cycfi.empty-starter");
    window _win(_app.name());
    _win.on_close = [&_app]() { _app.stop(); };
-
    view view_(_win);
 
+   auto open_button = button("Open Another Window", 1.0, bblue);
    view_.content(
-                     // Add more content layers here. The order
-                     // specifies the layering. The lowest layer
-                     // is at the bottom of this list.
-
-      background     // Replace background with your main element,
-                     // or keep it and add another layer on top of it.
+      align_center_middle(hmargin(50, link(open_button))),
+      background
    );
 
+   window win2("Another Window");
+   view view2(win2);
+   view2.content(box(bkd_color));
+   win2.hide();
+
+   open_button.on_click = [&](bool)
+   {
+      win2.show();
+   };
    _app.run();
    return 0;
 }
+
+
